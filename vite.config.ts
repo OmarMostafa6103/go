45c296aa8c19040f291a2905ecd8ec302300b12c
goLynk <noreply@golynk.com>
2026-02-28 14:19:31 +0200

    تحديث روابط الريبو بعد تغيير الاسم

--- vite.config.ts
+++ vite.config.ts
@@ -38,7 +38,7 @@ import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
 export default defineConfig({
-  base: "/go/",
+  base: "/golynk-website/",
   plugins: [tailwindcss()],
   resolve: {
     alias: {
